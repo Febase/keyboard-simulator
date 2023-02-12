8c5c0dd5b3542d8161669505795268fcc0359f70
behoney <be.honey@si-analytics.ai>
2023-02-13 03:43:26 +0900

    feat: onclick animation

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,9 +1,10 @@
-import { Vector3 } from '@react-three/fiber'
-import React from 'react'
+import { Vector3, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
 import KeyCap from './Key/KeyCap'
 import KeyLegend from './Key/KeyLegend'
+import { IkeyConfig } from '../../types/KeyboardType'
 
-const keyConfigs = [
+const keyConfigs: IkeyConfig[] = [
   {
     row: 0,
     column: 0,
@@ -36,20 +37,47 @@ interface IKeyboardProps {
   position: Vector3
 }
 
+const KEY_DOWN_POSITION = -0.5
+const KEY_DOWN_DURATION = 0.02
+
+type KeyProps = { keyConfig: IkeyConfig }
+const Key = (props: KeyProps) => {
+  const { keyConfig } = props
+  const [keyDownPosition, setKeyDownPosition] = useState<number>(0)
+
+  useFrame(() => {
+    keyDownPosition < 0 &&
+      setKeyDownPosition((prev) => prev + KEY_DOWN_DURATION)
+  })
+
+  return (
+    <group
+      onClick={(evt) => {
+        evt.stopPropagation()
+        setKeyDownPosition(KEY_DOWN_POSITION)
+      }}
+      position={[0, keyDownPosition, 0]}
+    >
+      <KeyCap
+        config={keyConfig}
+        position={[keyConfig.column, 0, keyConfig.row]}
+      />
+      <KeyLegend
+        config={keyConfig.legend}
+        keyPosition={{ row: keyConfig.row, column: keyConfig.column }}
+      />
+    </group>
+  )
+}
+
 export default ({ position }: IKeyboardProps) => {
   return (
     <group position={position}>
-      {keyConfigs.map((keyConfig) => (
-        <React.Fragment key={`${keyConfig.row}-${keyConfig.column}`}>
-          <KeyCap
-            config={keyConfig}
-            position={[keyConfig.column, 0, keyConfig.row]}
-          />
-          <KeyLegend
-            config={keyConfig.legend}
-            keyPosition={{ row: keyConfig.row, column: keyConfig.column }}
-          />
-        </React.Fragment>
+      {keyConfigs.map((keyConfig: IkeyConfig) => (
+        <Key
+          key={`${keyConfig.row}-${keyConfig.column}`}
+          keyConfig={keyConfig}
+        />
       ))}
     </group>
   )
