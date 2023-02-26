50945adfac097ab91ae76b91782643932f1d196a
behoney <be.honey@si-analytics.ai>
2023-02-26 13:14:57 +0900

    feat: simple case

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -3,12 +3,16 @@ import React, { useRef } from 'react'
 import { IkeyConfig } from '../../types/KeyboardType'
 import Key from './Key'
 import { keyConfigs } from './keyConfigs'
+import { Box } from '@react-three/drei'
+import { Color, MeshStandardMaterial } from 'three'
 
 interface IKeyboardProps {
   position: Vector3
 }
 
 export default ({ position }: IKeyboardProps) => {
+  const caseMaterial = new MeshStandardMaterial({ color: new Color('#C0C0C0') })
+
   return (
     <group position={position}>
       {keyConfigs.map((keyConfig: IkeyConfig) => (
@@ -17,6 +21,11 @@ export default ({ position }: IKeyboardProps) => {
           keyConfig={keyConfig}
         />
       ))}
+      <Box
+        position={[7.5, -0.35, 2.5]}
+        scale={[17, 0.55, 5.5]}
+        material={caseMaterial}
+      />
     </group>
   )
 }
