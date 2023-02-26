c4ee90609d2e977382754ca087ab365bd1145724
behoney <be.honey@si-analytics.ai>
2023-02-26 13:04:56 +0900

    refactor: move special config to new file

---
+++ src/components/Keyboard/specialKeyConfigs.tsx
@@ -0,0 +1,158 @@
+import { IkeyConfig } from '../../types/KeyboardType'
+
+export const specialKeyConfigs: IkeyConfig[] = [
+  {
+    row: 0,
+    column: 13,
+    rowSpan: 1,
+    colSpan: 2,
+    color: '#332C39',
+    legend: {
+      text: 'Backspace',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 1,
+    column: 0,
+    rowSpan: 1,
+    colSpan: 1.5,
+    color: '#332C39',
+    legend: {
+      text: 'Tab',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 1,
+    column: 13.5,
+    rowSpan: 1,
+    colSpan: 1.5,
+    color: '#332C39',
+    legend: {
+      text: '\\',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 2,
+    column: 0,
+    rowSpan: 1,
+    colSpan: 2,
+    color: '#332C39',
+    legend: {
+      text: 'Caps lock',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 2,
+    column: 13,
+    rowSpan: 1,
+    colSpan: 2,
+    color: '#332C39',
+    legend: {
+      text: 'Enter',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 3,
+    column: 0,
+    rowSpan: 1,
+    colSpan: 2.5,
+    color: '#332C39',
+    legend: {
+      text: 'Shift',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 3,
+    column: 12.5,
+    rowSpan: 1,
+    colSpan: 2.5,
+    color: '#332C39',
+    legend: {
+      text: 'Shift',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 0,
+    rowSpan: 1,
+    colSpan: 1.3,
+    color: '#332C39',
+    legend: {
+      text: 'Ctrl',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 1.3,
+    rowSpan: 1,
+    colSpan: 1.3,
+    color: '#332C39',
+    legend: {
+      text: 'Opt',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 2.6,
+    rowSpan: 1,
+    colSpan: 1.3,
+    color: '#332C39',
+    legend: {
+      text: 'Cmd',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 3.9,
+    rowSpan: 1,
+    colSpan: 7.1,
+    color: '#332C39',
+    legend: {
+      text: ' ',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 11,
+    rowSpan: 1,
+    colSpan: 1.3,
+    color: '#332C39',
+    legend: {
+      text: 'Cmd',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 12.3,
+    rowSpan: 1,
+    colSpan: 1.3,
+    color: '#332C39',
+    legend: {
+      text: 'Opt',
+      color: '#332C39',
+    },
+  },
+  {
+    row: 4,
+    column: 13.6,
+    rowSpan: 1,
+    colSpan: 1.4,
+    color: '#332C39',
+    legend: {
+      text: 'Ctrl',
+      color: '#332C39',
+    },
+  },
+]
